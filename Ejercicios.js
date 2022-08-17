1bd325ed198ea892524fa8e4b3e82f8e9fcde804
Flor098 <flor_cad09@hotmail.com>
2022-08-17 01:24:28 -0300

    commit teoria

--- Ejercicios.js
+++ Ejercicios.js
@@ -7,7 +7,7 @@ function Proto() {
 function agregarMetodoPrototype(Constructor) {
     // Agrega un método al Constructor del `prototype`
     // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
-    // Tu código:
+    // Tu código:dfdfdf
 
 }
 
